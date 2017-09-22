46b586d9a102d129b4fe99826e573ba1b15173d2
Amine195 <gmadhgs@gmail.com>
2017-09-22 10:44:10 +0100

    register page

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,41 @@
 $( document ).ready(function(){
     $(".button-collapse").sideNav();
+
+    $("#register").validate({
+        rules: {
+            username: {
+                required: true,
+                minlength: 5
+            },
+            email: {
+                required: true,
+                email:true
+            },
+            password: {
+                required: true,
+                minlength: 5
+            },
+            confirmation: {
+                required: true,
+                minlength: 5,
+                equalTo: "#password"
+            }
+        },
+        //For custom messages
+        messages: {
+            username:{
+                required: "Enter a username",
+                minlength: "Enter at least 5 characters"
+            }
+        },
+        errorElement : 'div',
+        errorPlacement: function(error, element) {
+          var placement = $(element).data('error');
+          if (placement) {
+            $(placement).append(error)
+          } else {
+            error.insertAfter(element);
+          }
+        }
+    });
 });
